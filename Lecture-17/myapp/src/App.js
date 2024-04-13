fd7c5c12ca3d0bece33f050cb8e2a950d963757d
mansi <vaghasiyamaahigmail.com>
2024-04-13 08:21:16 +0530

    13/04/2024

---
+++ Lecture-17/myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Useeffecttable from "./material-ui/Useeffect-table"
+
+function App() {
+  return (
+    <div className="App">
+<Useeffecttable/>
+    </div>
+  );
+}
+
+export default App;
