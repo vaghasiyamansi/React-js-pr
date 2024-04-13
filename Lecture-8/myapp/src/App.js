ac60d04cb634cb588fcf2e8379adcba02cdaedb4
mansi <vaghasiyamaahigmail.com>
2024-04-13 09:15:39 +0530

    13/04/2024

---
+++ Lecture-8/myapp/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Greeting from './Greeting-demo/Greeting';
+
+function App() {
+  return (
+    <Greeting/>
+  );
+}
+
+export default App;
